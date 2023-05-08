fe2d0c843c5e5bea1c5b4595b18bcfa981f5b758
shaun050 <112769676+shaun050@users.noreply.github.com>
2023-05-08 15:07:32 +0530

    Update query tickers.py

--- query tickers.py
+++ query tickers.py
@@ -1,26 +1,11 @@
 import requests
-import json
-#
-# Replace YOUR_API_KEY with your actual Alpha Vantage API key
-api_key = 'LEDOEFY0L5JHXGOA'
+import csv
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=LEDOEFY0L5JHXGOA'
 
-# Specify the URL for the Alpha Vantage API
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=LEDOEFY0L5JHXGOA'
-
-# Specify the parameters for the API request
-params = {
-    'function': 'LISTING_STATUS',
-    'apikey': api_key,
-}
-
-# Send the API request and retrieve the response
-response = requests.get(url, params=params)
-
-# Parse the JSON response
-data = json.loads(response.text)
-
-# Extract the list of symbols from the response
-symbols = data['data']
-
-# Print the list of symbols
-print(symbols)
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
